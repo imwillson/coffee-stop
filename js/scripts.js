e9b9cfb57fe91fecb56cb75cb4b5990b87668ca2
imwillson <liwillson1@gmail.com>
2018-03-15 15:42:19 -0400

    Print stop watch in console.log

--- js/scripts.js
+++ js/scripts.js
@@ -1,11 +1,72 @@
 function Stopwatch() {
-    this.start = function() {
-        console.log("watch is starting!");
-    };
-}
+    var time = 0;
+    var interval;
+    var offset;
 
-var watch = new Stopwatch();
-watch.start();
+    // runs every 10 miliseconds
+    function update() {
+    	time += delta();
+    	var formattedTime = timeFormatter(time);
+    	console.log(formattedTime);
+    };
+
+    function delta() {
+    	var now = Date.now();
+    	var timePassed = now - offset;
+
+    	// next time update is run, it'll used previous offset
+    	offset = now;
+
+    	return timePassed;
+    };
+    
+    function timeFormatter(timeInMilliseconds) {
+    	//Date is easier to convert
+    	var time = new Date(timeInMilliseconds);
+    	var minutes = time.getMinutes().toString();
+    	var seconds = time.getSeconds().toString();
+    	var milliseconds = time.getMilliseconds().toString();
+
+    	if (minutes.length < 2) {
+    		minutes = '0' + minutes;
+    	}
+
+    	if (seconds.length < 2){
+    		seconds = '0' + seconds;
+    	}
+
+    	while (milliseconds.length < 3) {
+    		milliseconds = '0' + milliseconds;
+    	}
+
+    	return minutes + ':' + seconds + '.' + milliseconds;
+    };
+
+
+    //closures
+    this.isOn = false; //always on off
+    this.start = function() {
+        if (!this.isOn) {
+        	interval = setInterval(update, 10);
+        	offset = Date.now();
+        	this.isOn = true;
+        }
+    };
+    this.stop = function() {
+    	if (this.isOn) {
+    		clearInterval(interval);
+    		interval = null;
+    		this.isOn = false;
+    	}
+    };
+    this.reset = function() {
+    	time = 0;
+    };
+    
+
+};
+
+
 
 $(document).ready(function(){
 
